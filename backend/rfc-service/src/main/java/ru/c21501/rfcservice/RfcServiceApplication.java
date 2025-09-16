409724b9dd33d52e72abfb8d866c0c5bd65f1b06
dmitriykosolobov <kosolobov.2003@gmail.com>
2025-09-16 21:24:31 +0300

    init spring project

---
+++ backend/rfc-service/src/main/java/ru/c21501/rfcservice/RfcServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.c21501.rfcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfcServiceApplication.class, args);
+	}
+
+}
---
+++ backend/rfc-service/src/test/java/ru/c21501/rfcservice/RfcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.c21501.rfcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
